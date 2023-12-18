48f6bc0fc8ca5af577c2b4ef8f12adc00631a561
Gabriel <gabrielarie2000@gmail.com>
2023-12-18 16:55:44 +0100

    Modify confi.yml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.ciapplication"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.ciapplication"
