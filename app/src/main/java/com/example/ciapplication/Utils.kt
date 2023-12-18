16a8a2ca0e00d0a1b627b767782e8a6c52df143b
Gabriel <gabrielarie2000@gmail.com>
2023-12-18 18:20:12 +0100

    add test

---
+++ app/src/main/java/com/example/ciapplication/Utils.kt
@@ -0,0 +1,8 @@
+package com.example.ciapplication
+
+class Utils {
+
+    public fun verifyPassword(pwd: String){
+
+    }
+}
---
+++ app/src/test/java/com/example/ciapplication/UtilskitTest.kt
@@ -0,0 +1,29 @@
+package com.example.ciapplication
+
+class UtilskitTest {
+/*
+    @Test
+    fun testValidPassword() {
+        val password = "Passw0rd!"
+
+        // Action
+        val results = verifyPassword(password)
+
+        // Result
+        assertTrue(results.isEmpty())
+    }
+
+    @Test
+    fun testlessThanSixCharPassword() {
+        val password = "Pwd!"
+
+        // Action
+        val results = verifyPassword(password)
+
+        // Result
+        assertEquals(results.size, 1)
+        assertTrue(results.isEmpty())
+    }
+    */
+
+}
